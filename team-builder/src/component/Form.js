1229885e0ba83fe16217cceede3474a5667ead10
Hector Valdez <hvaldez180@gmail.com>
2019-11-13 22:30:51 -0500

    added form

--- team-builder/src/component/Form.js
+++ team-builder/src/component/Form.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+    const [form, setForm] = useState({name:"", email:"", role:"",})
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const submitForm = event => {
+        event.preventDefault();
+        const newMem ={
+            ...form,
+            id:Date.now()
+        }
+        Props.addNewMem(newMem);
+        setForm({name: "", email: "", role: ""});
+    }
     return (
-        <form>
+        <form onSubmit={submitForm}>
             <label hmtlFor="name">name:</label>
-            <input type="text" placeholder="name"/>
+            <input 
+            type="text" 
+            placeholder="name"
+            name="name"
+            value={form.name}
+            onChange={changeHandler}/>
             <label htmlFor="email">Email:</label>
-            <input type="text" placeholder="email"/>
+            <input 
+            type="text" 
+            placeholder="email"
+            value={form.email}
+            onChange={changeHandler}/>
             <label htmlFor="role">Role:</label>
-            <input type="text" placeholder="role"/>
+            <input 
+            type="text" 
+            placeholder="role"
+            value={form.name}
+            onChange={changeHandler}/>
+            <button type="submit">Submit</button>
         </form>
     )
 }
---
+++ team-builder/src/component/FormInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function FormInfo() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default FormInfo;
